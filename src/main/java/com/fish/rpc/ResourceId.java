9436ef58f892960c1ddd8644ecb36a13e80eb923
wenxy@jugame.cn <wenxy@jugame.cn>
2018-01-23 17:52:44 +0800

    add http service support

---
+++ src/main/java/com/fish/rpc/ResourceId.java
@@ -0,0 +1,15 @@
+package com.fish.rpc;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Inherited
+@Retention(RetentionPolicy.RUNTIME) 
+@Target(ElementType.TYPE) 
+public @interface ResourceId {
+	String id();
+	String memo();
+}
